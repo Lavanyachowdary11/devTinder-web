e79393bee230c14d5f2f42c4c96532070ae0d042
Sai Lavanya Pothuru <46368765+Lavanyachowdary11@users.noreply.github.com>
2025-03-23 00:34:12 +0530

    build user card on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { BASE_URL } from "../utils/constants"
+import { useDispatch, useSelector } from 'react-redux'
+import { addFeed } from '../utils/feedSlice';
+import axios from "axios";
+import UserCard from './UserCard';
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed)
+  const dispatch = useDispatch();
+  const getFeed = async () => {
+    if(feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {withCredentials: true})
+      dispatch(addFeed(res?.data));
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+  
+  useEffect(() => {
+    getFeed();
+  }, [])
   return (
-    <div>Feed</div>
+    feed && (<div className='flex justify-center my-10'>
+      {feed.map(data => <UserCard user={data}/>
+    )}
+    </div>)
   )
 }
 
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReduer from "./userSlice"
+import feedReducer from "./feedSlice"
 
 const appStore = configureStore({
     reducer: {
-        user: userReduer
+        user: userReduer,
+        feed: feedReducer
     },
 });
 
